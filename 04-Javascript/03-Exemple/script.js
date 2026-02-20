0ffc2c5ccafa8eb006e1b3be796de020657ad9e3
optimisme <optimisme@gmail.com>
2026-02-20 15:49:23 +0100

    .

--- 04-Javascript/03-Exemple/script.js
+++ 04-Javascript/03-Exemple/script.js
@@ -1,36 +1,71 @@
 "use strict";
 
-const refFitxa = document.getElementById("refFitxa");
-const refReset = document.getElementById("refReset");
-const refInfo = document.getElementById("refInfo");
-
-const numPosicions = 5;
 const midaCasella = 75;
+const numFiles = 3;
+const numColumnes = 3;
 
-let posicioActual = 0;
+let posicioActual = {
+  fila: 0,
+  columna: 0
+};
 
-function init () {
-    refFitxa.addEventListener("click", mouADreta);
-    refReset.addEventListener("click", reset);
-    actualitzaFitxa();
-}
+function init() {
+  const refTauler = document.querySelector(".tauler");
+  const refReset = document.getElementById("refReset");
 
-function actualitzaFitxa() {
-  const posicioPixels = posicioActual * midaCasella;
+  // Definit els valors de les variables CSS
+  const refCSSRoot = document.documentElement;
+  refCSSRoot.style.setProperty("--mida", midaCasella + "px");
+  refCSSRoot.style.setProperty("--files", numFiles);
+  refCSSRoot.style.setProperty("--columnes", numColumnes);
 
-  refFitxa.style.transform = `translateX(${posicioPixels}px)`;
-  refInfo.textContent = `Posició: ${posicioActual} / 4 (${posicioPixels}px)`;
-}
 
-function mouADreta() {
-  if (posicioActual < numPosicions - 1) {
-    posicioActual++;
-    actualitzaFitxa();
+
+  // Afegir caselles al tauler
+  for (let fila = 0; fila < numFiles; fila++) {
+    for (let columna = 0; columna < numColumnes; columna++) {
+      const refCasella = document.createElement("div");
+      refCasella.classList.add("casella");
+      refCasella.addEventListener("click", () => mouFitxa(fila, columna));
+      refCasella.textContent = `${fila * numColumnes + columna}`;
+      refCasella.style.left = `${columna * midaCasella}px`;
+      refCasella.style.top = `${fila * midaCasella}px`;
+      refTauler.appendChild(refCasella);
+    }
   }
-}
 
-function reset() {
-  posicioActual = 0;
+  // Afegir fitxa al tauler
+  var refFitxa = document.createElement("div");
+  refFitxa.classList.add("fitxa");
+  refTauler.appendChild(refFitxa);
+
+  // Afegir event al botó de reset
+  refReset.addEventListener("click", reset);
+
   actualitzaFitxa();
 }
 
+function mouFitxa(fila, columna) {
+  // Actualitzar posició actual
+  posicioActual.fila = fila;
+  posicioActual.columna = columna;
+
+  // Moure fitxa a la nova posició
+  posicionaFitxa();
+}
+
+function posicionaFitxa() {
+  const refFitxa = document.querySelector(".fitxa");
+  const refInfo = document.getElementById("refInfo");
+
+  refFitxa.style.transform = `translate(${posicioActual.columna * midaCasella}px, ${posicioActual.fila * midaCasella}px)`;
+  refInfo.textContent = `Fila: ${posicioActual.fila}, Columna: ${posicioActual.columna}`;
+}
+
+function reset() {
+
+  posicioActual.fila = 1;
+  posicioActual.columna = 1;
+
+  posicionaFitxa();
+}
